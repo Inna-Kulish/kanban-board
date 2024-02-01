5f41f28a4801392c2cffeab3a3a28ff7d4064b3e
Inna-Kulish <norelajest@gmail.com>
2024-02-01 11:24:42 +0200

    securing token

--- src/redux/issues/operations.ts
+++ src/redux/issues/operations.ts
@@ -1,20 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { token } from "./token";
-
-const instance = axios.create({
-  baseURL: "https://api.github.com/repos",
-  headers: {
-    Accept: "application/vnd.github.v3+json",
-    Authorization: `Bearer ${token}`,
-  },
-});
+import { axiosInstance } from "./axios";
 
 export const fetchIssues = createAsyncThunk(
   "issues/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const result = await instance.get("/facebook/react/issues");
+      const result = await axiosInstance.get("/facebook/react/issues");
       console.log(result.data);
       return result.data;
     } catch (e) {
